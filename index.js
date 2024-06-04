60c1a87760553fa22cabff9c6db04f39b45a2f27
Romain Avalle <romain.avalle@lg2.com>
2024-06-04 11:45:49 -0400

    new nuxt hook

--- index.js
+++ index.js
@@ -1,84 +1,83 @@
 import linkResolverJSON from "./../../../assets/jsons/link-resolver.json";
-import { defineNuxtModule } from '@nuxt/kit'
+import { defineNuxtModule } from "@nuxt/kit";
 
-const fs = require('fs')
+const fs = require("fs");
 
 async function checkHeadersFile() {
-  const headersFilePath = './public/_headers'
-  const lineToAdd = '\n/dato-route-map.json\n  Access-Control-Allow-Origin: *\n'
+  const headersFilePath = "./public/_headers";
+  const lineToAdd =
+    "\n/dato-route-map.json\n  Access-Control-Allow-Origin: *\n";
   if (fs.existsSync(headersFilePath)) {
-    const fileContent = fs.readFileSync(headersFilePath, 'utf8')
+    const fileContent = fs.readFileSync(headersFilePath, "utf8");
     if (!fileContent.includes(lineToAdd)) {
-      return await fs.appendFileSync(headersFilePath, lineToAdd)
+      return await fs.appendFileSync(headersFilePath, lineToAdd);
     }
   } else {
-    return await fs.writeFileSync(headersFilePath, lineToAdd)
+    return await fs.writeFileSync(headersFilePath, lineToAdd);
   }
 }
 async function checkJSONFile(array) {
-  const filePath = './public/dato-route-map.json'
+  const filePath = "./public/dato-route-map.json";
   // Check if the file exists
   return await fs.access(filePath, fs.constants.F_OK, async (err) => {
-    console.log(err)
+    console.log(err);
     if (!err) {
       // File exists, so delete it
       await fs.unlink(filePath, (err) => {
         if (err) {
-          console.error('Error deleting file:', err)
+          console.error("Error deleting file:", err);
         } else {
-          console.log('File deleted successfully.')
+          console.log("File deleted successfully.");
         }
-      })
+      });
     }
     return await fs.writeFile(filePath, JSON.stringify(array), (err) => {
       if (err) {
-        console.error(err)
+        console.error(err);
       }
-    })
-  })
+    });
+  });
 }
 
 export default defineNuxtModule({
   setup(options, nuxtApp) {
-    nuxtApp.hooks.hook('pages:extend', async (pages) => {
-      // console.log('pages:extend', pages)
+    nuxtApp.hooks.hook("build:done", async () => {
+      const array = [];
+      nuxtApp.apps.default.pages.forEach((route) => {
+        // console.log(route)
+        const entryArray = Object.entries(linkResolverJSON).find(
+          (entry) =>
+            (entry[1].name === route.name) |
+            (entry[1].name === route.name.split("___")[0])
+        );
 
-      const array = []
-      pages.forEach((route) => {
-          // console.log(route)
-          const entryArray = Object.entries(linkResolverJSON).find(
-            (entry) =>
-              (entry[1].name === route.name) |
-              (entry[1].name === route.name.split('___')[0]),
-          )
-
-          if (entryArray) {
-            const entry = {
-              path: route.path,
-              params: [],
-              model: entryArray[0],
-            }
-            if (entryArray[1].params) {
-              Object.entries(entryArray[1].params).forEach((param) => {
-                entry.params.push(param[1])
-                entry.path = entry.path.replace(param[0], param[1])
-              })
-            }
-            if (route.name.includes(`___`)) {
-              entry.locale = route.name.split(`___`)[1]
-            }
-            entry.path = entry.path.replaceAll('()', '')
-            array.push(entry)
+        if (entryArray) {
+          const entry = {
+            path: route.path,
+            params: [],
+            model: entryArray[0],
+          };
+          if (entryArray[1].params) {
+            Object.entries(entryArray[1].params).forEach((param) => {
+              entry.params.push(param[1]);
+              entry.path = entry.path.replace(param[0], param[1]);
+            });
           }
-        })
+          if (route.name.includes(`___`)) {
+            entry.locale = route.name.split(`___`)[1];
+          }
+          entry.path = entry.path.replaceAll("()", "");
+          array.push(entry);
+        }
+      });
       // write a function that check if the file ./public/headers exists. If it exists check if the file as already a ligne with '/dato-route-map.json' if not add it. If the file do not exist create it and add the ligne '/dato-route-map.json' in it.
 
-      await checkHeadersFile()
-      await checkJSONFile(array)
-      console.log('dato route map - DONE!')
-      console.table(array)
-      return true
+      await checkHeadersFile();
+      await checkJSONFile(array);
+      console.log("dato route map - DONE!");
+      console.table(array);
+      return true;
       // })
-    })
+    });
   },
-})
+});
